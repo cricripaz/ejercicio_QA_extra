fdc47829c882ec43b77ebb66d60cfeb60661d54d
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-02-22 08:20:45 -0400

    SEGUNDO COMMIT

--- src/main/java/Beca.java
+++ src/main/java/Beca.java
@@ -1,2 +1,28 @@
 public class Beca {
+
+    Utils utils;
+    Helpers helpers;
+
+
+    String msg = "";
+
+    public Beca(Utils utilsMockeado,Helpers helpersMockeado){
+
+        utils = utilsMockeado;
+        helpers = helpersMockeado;
+
+
+    }
+
+
+
+    public String recomendacionBeca(int CI){
+
+        if (Helpers.aplicaBeca(CI)){
+           msg =  utils.getNota(CI)
+
+
+        }
+
+    }
 }
