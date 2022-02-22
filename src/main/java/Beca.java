ec7bf3b7998ba573c766e8e6b3683355c9ed5769
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-02-22 08:30:43 -0400

    TERCER COMMIT

--- src/main/java/Beca.java
+++ src/main/java/Beca.java
@@ -14,15 +14,29 @@ public class Beca {
 
     }
 
+    public Beca(){}
+
 
 
     public String recomendacionBeca(int CI){
 
+
+        int notaTemp = 0 ;
         if (Helpers.aplicaBeca(CI)){
-           msg =  utils.getNota(CI)
+            notaTemp = utils.getNota(CI);
+
+            if (notaTemp >= 90){
+                msg = "SI APLICA A BECA";
+            }else {
+                msg = "NO APLICA A BECA POR PROMEDIO ACADEMICO";
+            }
 
 
+        }else {
+            msg = "EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS";
         }
+
+        return msg;
 
     }
 }
